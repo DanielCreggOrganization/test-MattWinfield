d47690f3d3cfa7cf365c0ad8be1a1e36ee26e45b
Matthew Winfield <g00373280@gmit.ie>
2020-01-27 17:47:50 +0000

    Added Lecture and Lab for Week3

---
+++ src/ie/gmit/labweek3/Data.java
@@ -0,0 +1,31 @@
+package ie.gmit.labweek3;
+
+
+class Data{
+
+
+    public static void main(String[] args) {
+        boolean isAdult = true;
+        byte age = 1;
+        short populationOfVillage = 1;
+        int phoneNumber = 1;
+        long numberOfMoleculesInCm3H2O = 334500000000000000L;
+        char gender = 'M';
+        float percentageOfGrade = 92.68f;
+        double piTo10Places = 3.1415926535;
+
+
+        System.out.println(isAdult);
+        System.out.println(age);
+        System.out.println(populationOfVillage);
+        System.out.println(phoneNumber);
+        System.out.println(numberOfMoleculesInCm3H2O);
+        System.out.println(gender);
+        System.out.println(percentageOfGrade);
+        System.out.println(piTo10Places);
+
+    }
+
+
+
+}
---
+++ src/ie/gmit/lectweek3/Car.java
@@ -0,0 +1,46 @@
+package ie.gmit.lectweek3;
+
+public class Car{
+
+
+
+
+    public static void main(String[] args) {
+        
+
+
+    }
+
+    int topSpeed;
+    int totalSeats;
+    int fuelCapacity;
+    String manufacturer;
+    String owner;
+    String colour;
+    float engineSize;
+    float value;
+
+    void show(){
+        System.out.println(manufacturer);
+    }
+
+    void refuel(){
+
+    }
+
+    void park(){
+
+
+    }
+
+    void drive(){
+
+
+    }
+
+    void crash(){
+        value = 0F;
+
+    }
+
+}
